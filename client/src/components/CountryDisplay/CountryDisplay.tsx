eacc71bc37a0b5329843eb591f7a96cb8228b8ef
Shehan Abeyrathne <shehan.abeyrathne96@gmail.com>
2022-05-17 12:45:33 +0530

    Update CountryDisplay.tsx

--- client/src/components/CountryDisplay/CountryDisplay.tsx
+++ client/src/components/CountryDisplay/CountryDisplay.tsx
@@ -1,17 +1,27 @@
 import { useState } from 'react';
 import { TeamOutlined } from '@ant-design/icons';
+import { BASE_CURRENCY } from '../../constants/Constants';
 import classes from './CountryDisplay.module.scss';
+import { Currency } from '../../models/Currency.model';
 
 export const CountryDisplay = (props: any) => {
-  const [localCurrencyValue, setLocalCurrencyValue] = useState(1);
+  const [sekCurrencyValue, setLocalCurrencyValue] = useState(1);
 
-  const currencyConvertor = () => {
-    let amount: number = +props.amountInSek;
+  const currencyConvertor = (exchangeRate?: string) => {
+    var amount: number = +props.amountInSek;
+    var rate: number = exchangeRate == undefined ? 0 : +exchangeRate;
     if (amount == 0 || amount == null || amount == undefined) {
       return 0;
     } else {
-      return (amount / props.exchangeRateToSek).toFixed(2);
+      return (amount * rate).toFixed(2);
     }
+  };
+
+  const getCurrencyRelatedValues = (currencies: Currency[], currencyCode: string) => {
+    var currencyObject = currencies.find((object) => {
+      return object.code === currencyCode;
+    });
+    return currencyObject;
   };
 
   return (
@@ -24,34 +34,47 @@ export const CountryDisplay = (props: any) => {
           <TeamOutlined />
           <span>{props.population}</span>
         </h3>
-        <h3 className={classes.CurrencyCode}></h3>
-        <table className={classes.CurrencyContainer}>
-          <thead>
-            <tr>
-              <th>{props.currencyCode}</th>
-              <th>SEK</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{localCurrencyValue}</td>
-              <td>{props.exchangeRateToSek}</td>
-            </tr>
-          </tbody>
-        </table>
-        <div className={classes.divHeight}></div>
-        <table className={classes.CurrencyContainer}>
-          <thead>
-            <tr>
-              <th>CONVERTED AMOUNT IN {props.currencyCode}</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{currencyConvertor()}</td>
-            </tr>
-          </tbody>
-        </table>
+        {props.currencyCodes.map((localCurrencyCode: string) => (
+          <table key={localCurrencyCode} className={classes.CurrencyContainer}>
+            <thead>
+              <tr>
+                <th>{BASE_CURRENCY}</th>
+                <th>{localCurrencyCode}</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{sekCurrencyValue}</td>
+                <td>
+                  {getCurrencyRelatedValues(
+                    props.currencies,
+                    localCurrencyCode
+                  )?.exchange_rate.toFixed(2)}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        ))}
+        {props.currencyCodes.map((localCurrencyCode: string) => (
+          <table key={localCurrencyCode} className={classes.CurrencyContainer}>
+            <thead>
+              <tr>
+                <th>CONVERTED AMOUNT IN {localCurrencyCode}</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  {currencyConvertor(
+                    getCurrencyRelatedValues(props.currencies, localCurrencyCode)
+                      ?.exchange_rate.toFixed(2)
+                      .toString()
+                  )}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        ))}
       </div>
     </>
   );
