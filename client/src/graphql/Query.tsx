a4e0c0bf459dfe5f4d3b8586fa260931da820cb0
Shehan Abeyrathne <shehan.abeyrathne96@gmail.com>
2022-05-17 12:45:22 +0530

    Update Query.tsx

--- client/src/graphql/Query.tsx
+++ client/src/graphql/Query.tsx
@@ -3,13 +3,14 @@ import { gql } from '@apollo/client';
 export const GET_COUNTRY_QUERY = gql`
   query GetCountryDetails($searchCountry: String!) {
     GetCountryDetails(searchCountry: $searchCountry) {
-      population
-      official_name
       common_name
+      official_name
       flag
-      currency_code
-      currency {
-        exchange_rate_to_sek
+      population
+      currency_codes
+      currencies {
+        code
+        exchange_rate
       }
     }
   }
