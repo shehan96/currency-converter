07dddd002c6bb4dbbbe602c07b1396232e5d721e
Shehan Abeyrathne <shehan.abeyrathne96@gmail.com>
2022-06-02 20:02:53 +0530

    Update CurrencyConvertor.ts

--- client/src/utils/CurrencyConvertor.ts
+++ client/src/utils/CurrencyConvertor.ts
@@ -2,7 +2,7 @@
  * Currency convertor function which convert amount in sek to local currency
  * @params amountInSek, exchangeRate
  * */
-export const CurrencyConvertor = (amountInSek: any, exchangeRate?: string) => {
+export const CurrencyConvertor = (amountInSek: number, exchangeRate?: string) => {
   var amount: number = +amountInSek;
   var rate: number = exchangeRate == undefined ? 0 : +exchangeRate;
   if (amount == 0 || amount == null || amount == undefined) {
